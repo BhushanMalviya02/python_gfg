39653602630ee979d72e0f2711faae5b211f16df
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-02-03 12:10:37 +0530

    problem solved 3/2/26

---
+++ p7_max subbry.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubarraySum(self, arr):
+        # Code here
+        tempsum=0
+        maxsum=arr[0]
+        for i in range(len(arr)):
+            tempsum+=arr[i]
+            maxsum=max(maxsum,tempsum)
+            
+            if tempsum<0:
+                tempsum=0
+                
+            
+        return maxsum
