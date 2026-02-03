3d51ce414768e556a30f9599a9040815e099ea9b
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-02-03 11:59:03 +0530

    problem solved 3/2/26

---
+++ p5_largest in arr.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largest(self, arr):
+        # code here
+        lar=arr[0]
+        for i in range (len(arr)):
+            if lar<arr[i]:
+                lar=arr[i]
+                
+        return lar
+    
+                
+   
+        
